b490a4ece3642b727bb06ab73a1b77df903ee4ac
Anton Samoshin <asamoshin9@gmail.com>
2019-02-11 16:27:21 +0300

    Add differ engine

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,12 @@
 #! /usr/bin/env node
+import program from 'commander';
+import differ from '../differ-engine';
 
-console.log('Hello gendiffer!');
+program.description('Compares two configuration files and shows a difference.')
+  .version('0.0.8')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format');
+
+program.parse(process.argv);
+
+differ();
