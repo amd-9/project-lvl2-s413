eee61d4410f6846b1a5eddd28bd64de1c2b4d5e1
Anton Samoshin <asamoshin9@gmail.com>
2019-02-11 16:49:16 +0300

    Fix test config

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,7 @@ import program from 'commander';
 import differ from '../differ-engine';
 
 program.description('Compares two configuration files and shows a difference.')
-  .version('0.0.9')
+  .version('0.0.10')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format');
 
