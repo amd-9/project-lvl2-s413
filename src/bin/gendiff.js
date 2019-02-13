53f40f6068d98c6bb5284f77ee5aa1def6f3256e
Anton Samoshin <asamoshin9@gmail.com>
2019-02-13 17:15:14 +0300

    Add json config differ

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,12 +3,13 @@ import program from 'commander';
 import differ from '../differ-engine';
 
 program.description('Compares two configuration files and shows a difference.')
-  .version('0.0.10')
+  .version('0.0.11')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'Output format');
+  .option('-f, --format [type]', 'Output format')
+  .action((firstConfig, secondConfig) => {
+    if (firstConfig && secondConfig) {
+      console.log(differ(firstConfig, secondConfig));
+    }
+  });
 
 program.parse(process.argv);
-
-if (program.format) {
-  console.log(`selected format:  ${differ(program.format)}`);
-}
--- src/differ-engine.js
+++ src/differ-engine.js
@@ -1,1 +1,23 @@
-export default type => type;
+import fs from 'fs';
+import path from 'path';
+
+export default (pathToConfig1, pathToConfig2) => {
+  const firstConfig = JSON.parse(fs.readFileSync(path.resolve(pathToConfig1)));
+  const secondConfig = JSON.parse(fs.readFileSync(path.resolve(pathToConfig2)));
+
+  const reducer = (acc, key) => {
+    if (Object.prototype.hasOwnProperty.call(secondConfig, key)) {
+      return firstConfig[key] !== secondConfig[key]
+        ? { ...acc, [`+ ${key}`]: secondConfig[key], [`- ${key}`]: firstConfig[key] } : { ...acc, [`  ${[key]}`]: firstConfig[key] };
+    }
+    return { ...acc, [`- ${key}`]: firstConfig[key] };
+  };
+
+  const deletedOrChanged = Object.keys(firstConfig).reduce(reducer, {});
+  const added = Object.keys(secondConfig).reduce((acc, key) => (
+    Object.prototype.hasOwnProperty.call(firstConfig, key)
+      ? acc : { ...acc, [`+ ${key}`]: secondConfig[key] }), {});
+  const diffResult = { ...deletedOrChanged, ...added };
+
+  return `{${Object.keys(diffResult).reduce((acc, key) => `${acc}\n  ${key}: ${diffResult[key]}`, '')}\n}`;
+};
