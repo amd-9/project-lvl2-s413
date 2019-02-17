79745139832ed4b5760df9de960f224833035696
Anton Samoshin <asamoshin9@gmail.com>
2019-02-17 17:53:04 +0300

    Update package version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,7 @@ import program from 'commander';
 import generateDiff from '..';
 
 program.description('Compares two configuration files and shows a difference.')
-  .version('0.5.2')
+  .version('0.5.3')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'Output format', 'generic')
   .action((firstConfig, secondConfig) => {
